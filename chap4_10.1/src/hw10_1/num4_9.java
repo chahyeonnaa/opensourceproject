95f9baf61c2d19d2a5874f796fa6ccea7775966e
chahyeonnaa <namuemue0509@gmail.com>
2021-10-01 17:15:07 +0900

    10월 1일 실습

---
+++ chap4_10.1/src/hw10_1/num4_9.java
@@ -0,0 +1,13 @@
+package hw10_1;
+
+public class num4_9 {
+    public static void main(String[] args) {
+        String a = new String("Good");
+        String b = new String("Bad");
+        String c = new String("Normal");
+        String d, e;
+        a = null;
+        d = c;
+        c = null;
+    }
+}
